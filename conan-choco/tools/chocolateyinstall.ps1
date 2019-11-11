8123f5bf09509179c845544a8132316db5241cdf
Edgar <Edgar@AnotherFoxGuy.com>
2019-11-11 16:54:03 +0100

    :arrow_up: Update conan to 1.20.3

--- conan-choco/tools/chocolateyinstall.ps1
+++ conan-choco/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.bintray.com/conan/installers/conan-win-32_1_20_2.exe'
-$url64      = 'https://dl.bintray.com/conan/installers/conan-win-64_1_20_2.exe'
+$url        = 'https://dl.bintray.com/conan/installers/conan-win-32_1_20_3.exe'
+$url64      = 'https://dl.bintray.com/conan/installers/conan-win-64_1_20_3.exe'
 
 # https://hash.online-convert.com/sha256-generator
 
@@ -11,9 +11,9 @@ $packageArgs = @{
   url            = $url
   url64bit       = $url64
   softwareName   = 'Conan'
-  checksum       = '86975F9132D8AB74E72AFFDE6930DEE034D8769379E3FCCD5114E5C1EFE3119B'
+  checksum       = 'A66CDC961ED198B2952B09374ACB0E5FD9AD3589F24F2955B3C70F7AB9B1991E'
   checksumType   = 'sha256'
-  checksum64     = '2F85E798B8CC4B919F73E6851A1C82A2892D79C49D9E4E8F660D1DB82430F91B'
+  checksum64     = '4383361E84BAC1BFD72120D039F972D1D406CF15A0E463ADEB8523ACF733E7B9'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT'
 }

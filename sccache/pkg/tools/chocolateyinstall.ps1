dc7a4f62c28368c38e6731332313225e63b4f9a9
Edgar <Edgar@AnotherFoxGuy.com>
2019-12-26 17:29:01 +0100

    :bug: Fixed some issues with sccache

--- sccache/pkg/tools/chocolateyinstall.ps1
+++ sccache/pkg/tools/chocolateyinstall.ps1
@@ -3,6 +3,6 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile -PackageName 'sccache'  -FileFullPath "$toolsDir\sccache-0.2.12-x86_64-pc-windows-msvc.tar.gz" -Url 'https://github.com/mozilla/sccache/releases/download/0.2.12/sccache-0.2.12-x86_64-pc-windows-msvc.tar.gz'
+Get-ChocolateyWebFile -PackageName 'sccache' -FileFullPath "$toolsDir\sccache-0.2.12-x86_64-pc-windows-msvc.tar.gz" -Url 'https://github.com/mozilla/sccache/releases/download/0.2.12/sccache-0.2.12-x86_64-pc-windows-msvc.tar.gz' -Checksum 'FD05E91C59B9497D4EBAE311B47A982F2A6EB942DCA3C9C314CC1FB36F8BC64D' -ChecksumType 'sha256'
 Get-ChocolateyUnzip -FileFullPath "$toolsDir\sccache-0.2.12-x86_64-pc-windows-msvc.tar.gz" -Destination $toolsDir
-Install-ChocolateyZipPackage -PackageName 'sccache' -File "$toolsDir\sccache-0.2.12-x86_64-pc-windows-msvc.tar" -UnzipLocation $toolsDir
+Get-ChocolateyUnzip -PackageName 'sccache' -FileFullPath "$toolsDir\sccache-0.2.12-x86_64-pc-windows-msvc.tar" -Destination $toolsDir

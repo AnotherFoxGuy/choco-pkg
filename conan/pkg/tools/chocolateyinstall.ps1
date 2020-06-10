23399829ffdc80fcf56d9454b898037580c087c6
Edgar <Edgar@AnotherFoxGuy.com>
2020-06-10 12:05:54 +0200

    :arrow_up: Update conan to 1.26.0

--- conan/pkg/tools/chocolateyinstall.ps1
+++ conan/pkg/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-# Install script for conan V1.25.2
+# Install script for conan V1.26.0
 
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.bintray.com/conan/installers/conan-win-32_1_25_2.exe'
-$url64      = 'https://dl.bintray.com/conan/installers/conan-win-64_1_25_2.exe'
+$url        = 'https://dl.bintray.com/conan/installers/conan-win-32_1_26_0.exe'
+$url64      = 'https://dl.bintray.com/conan/installers/conan-win-64_1_26_0.exe'
 
 $packageArgs = @{
   packageName    = 'Conan'
@@ -12,9 +12,9 @@ $packageArgs = @{
   url            = $url
   url64bit       = $url64
   softwareName   = 'Conan'
-  checksum       = 'EBEE946B64197DDE84B10D0B65467CFB422926D1FEDE28877DBA92BDC2D1EDEC'
+  checksum       = 'AAD758F31FF965699D35021EF948A6E852CC08038C1E7F0D7224F01FD61F261F'
   checksumType   = 'sha256'
-  checksum64     = 'F9EC0AB78F9B62241FB090AE4F247D678FE9775BD7A390FA15CE94C983D9F5E6'
+  checksum64     = 'DC953D128700A33FD8F7876E341172C9B2083F5392B318CCB2692203CF7BFDC0'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT'
 }
--- conan/pkg/tools/chocolateyuninstall.ps1
+++ conan/pkg/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-# Uninstall script for conan V1.25.2
+# Uninstall script for conan V1.26.0
 
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{

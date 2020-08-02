89130a5fd048f5a6c8b226a48625ec76f9b83da9
Edgar <Edgar@AnotherFoxGuy.com>
2020-08-02 10:59:49 +0200

    :arrow_up: Update butler to 15.19

--- butler/pkg/tools/chocolateyinstall.ps1
+++ butler/pkg/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-# Install script for butler V15.18.0
+# Install script for butler V15.19.0
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName 'butler' `
-    -Url 'https://broth.itch.ovh/butler/windows-amd64/15.18.0/archive/default#/latest.zip' `
+    -Url 'https://broth.itch.ovh/butler/windows-amd64/15.19.0/archive/default#/latest.zip' `
     -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
-    -Checksum '5D1D397521817DFEB9285E6FD820CD935FF727D57607B7CE166A4807A06EE3CA' -ChecksumType 'sha256'
+    -Checksum '314FD33D07CAD03E27C861225B3BE0AD16C2E10C26E34406B7BF4BBA0597923E' -ChecksumType 'sha256'

c9b73a9644f12ef36a6605282a346608ae62b219
Edgar <Edgar@AnotherFoxGuy.com>
2023-10-29 12:43:30 +0100

    :arrow_up: Update sccache to 0.6.0

--- sccache/pkg/tools/chocolateyinstall.ps1
+++ sccache/pkg/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
-# Install script for sccache V0.5.4
+# Install script for sccache V0.6.0
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile -PackageName 'sccache' -FileFullPath "$toolsDir\sccache-v0.5.4-x86_64-pc-windows-msvc.zip" -Url 'https://github.com/mozilla/sccache/releases/download/v0.5.4/sccache-v0.5.4-x86_64-pc-windows-msvc.zip' -Checksum '56907A176F87D9FC62B0392B82E650B06C8B056281B70C42A62DC9D955BFBF4F' -ChecksumType 'sha256'
-Get-ChocolateyUnzip -PackageName 'sccache' -FileFullPath "$toolsDir\sccache-v0.5.4-x86_64-pc-windows-msvc.zip" -Destination $toolsDir
+Get-ChocolateyWebFile -PackageName 'sccache' -FileFullPath "$toolsDir\sccache-v0.6.0-x86_64-pc-windows-msvc.zip" -Url 'https://github.com/mozilla/sccache/releases/download/v0.6.0/sccache-v0.6.0-x86_64-pc-windows-msvc.zip' -Checksum '387CA4DC85BA5AFFEE1C40ABC18EB17BE5158E1B51706ACE212BEF703CCDF246' -ChecksumType 'sha256'
+Get-ChocolateyUnzip -PackageName 'sccache' -FileFullPath "$toolsDir\sccache-v0.6.0-x86_64-pc-windows-msvc.zip" -Destination $toolsDir
